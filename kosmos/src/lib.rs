b1002e28df4e2a1f3aaae806d4ebea52d1f71f8b
gyara <me@gyara.moe>
2020-03-10 16:31:29 +0900

    xeno client

--- kosmos/src/lib.rs
+++ kosmos/src/lib.rs
@@ -4,3 +4,4 @@ pub mod client;
 mod planet;
 pub mod server;
 pub mod utils;
+pub mod xeno;
---
+++ kosmos/src/xeno/client.rs
@@ -0,0 +1,58 @@
+use crate::{client::UnixClient, utils::*};
+use async_std::{os::unix::net::UnixStream, prelude::*, task};
+use async_trait::async_trait;
+use serde::de::DeserializeOwned;
+
+#[async_trait]
+pub trait XenoHandler: Send + Sync + Clone {
+    type In: DeserializeOwned + Send + Sync;
+    type Out: Package;
+
+    async fn handle(&self, input: Self::In) -> anyhow::Result<Option<Self::Out>>;
+}
+
+pub struct XenoClient<T: XenoHandler + 'static> {
+    client: UnixClient,
+    handler: T,
+}
+
+async fn recv_in<T: XenoHandler + 'static>(handler: &T, stream: &mut UnixStream) -> anyhow::Result<Status> {
+    let input: ReadResult<T::In> = stream.unpack().await?;
+    if let ReadResult::Continue(input) = input {
+        let output = handler.handle(input).await?;
+        stream.send(&output).await?;
+    } else {
+        return Ok(Status::Exit);
+    }
+    Ok(Status::Continue)
+}
+
+impl<T: XenoHandler> XenoClient<T> {
+    pub fn new<N: Into<String>>(name: N, handler: T) -> Self {
+        let client = UnixClient::new(name);
+        Self { client, handler }
+    }
+
+    pub async fn run(&mut self) -> anyhow::Result<()> {
+        let listener = self.client.listen().await?;
+        let mut incoming = listener.incoming();
+        while let Some(stream) = incoming.next().await {
+            let handler = self.handler.clone();
+            task::spawn(async move {
+                let mut stream = stream.unwrap();
+                loop {
+                    match recv_in(&handler, &mut stream).await {
+                        Ok(Status::Continue) => {},
+                        Ok(Status::Exit) => {break},
+                        Err(e) => {
+                            eprintln!("Xeno Error: {}", e);
+                            break;
+                        }
+
+                    }
+                }
+            });
+        }
+        Ok(())
+    }
+}
